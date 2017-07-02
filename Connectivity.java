26f1c6c2f095d08ba3c4b23f2c133e45e461c1d4
Shashanksingh360 <shashank.shashank.singh360@gmail.com>
2017-07-02 12:17:20 +0530

    Update Connectivity.java

--- Connectivity.java
+++ Connectivity.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.sql.*;
 
 
@@ -8,8 +9,9 @@ class Connectivity{
 	void connection()
 	{
        try{     
-		Class.forName("oracle.jdbc.driver.OracleDriver"); 
-		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","system","shashank");
+    	   
+		Class.forName("com.mysql.jdbc.Driver"); 
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/CMS?useSSL=false","root","shashank");
 		}
 		catch(SQLException e)
 		{
@@ -21,37 +23,62 @@ class Connectivity{
 	}
 	
 	public void loginVerification(String lusername,String lpassword){
-		      
-		     lusername=lusername.toLowerCase();
-		     String password=null;
+		    lusername=lusername.toLowerCase();
+		    String password=null;
 		try{
-			PreparedStatement st= con.prepareStatement("select * from student where NAME=?");
+			PreparedStatement st= con.prepareStatement("select password from user where username=?");
 			st.setString(1, lusername);
 			ResultSet rs=st.executeQuery();   
-			
 			while(rs.next())
 			{
-				
-				password=rs.getString(3);
-				
+			password=rs.getString(1);
 			}
-			
 			if(lpassword.equals(password))
 			{
 				System.out.println("Successful");
 				Userpage uo=new Userpage();
 				Logintest.f.setVisible(false);
 				uo.user();
-				
-			}
+				}
 			else{
 				System.out.println("Failed");
+				
 			}
-			
 		}
 		catch(SQLException e){
-			//System.out.println(e);
-			}
+		}
+	}
+	
+	public void registration(String rfname,String rlname, String rusername, String rpassword ,String rrole, String rgender)
+	{
+		rusername=rusername.toLowerCase();
+		
+		try {
+			PreparedStatement st=con.prepareStatement("insert into user values(?,?,?,?,?,?,?)");
+			st.setString(1, rfname);
+			st.setString(2, rlname);
+			st.setString(3, rusername);
+			st.setString(4, rpassword);
+			st.setString(5, rrole);
+			st.setString(6, rgender);
+			st.setString(7, "Not Approved");
+			//int i=st.executeUpdate();
+			
+		}
+		catch (SQLException e) {
+		}
+		
+	}
+	
+	public void approved()
+	{
+		try{
+			PreparedStatement st= con.prepareStatement("select * from user where username=?");
+			//st.setString(1, lusername);
+			ResultSet rs=st.executeQuery();   
+		}
+		catch(SQLException e){
+		}
 		
 	}
 }
